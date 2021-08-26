121529b32977e24a7c73313b57a3330433ee71a6
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-08-26 16:04:13 -0500

    Updated the db connection string with my local database. Following along with module 3 (Testing Model Classes with xUnit.NET). Added FrequentFlyerNumberValidator.cs model and accompanying tests.

---
+++ src/CreditCards/Core/Model/FrequentFlyerNumberValidator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditCards.Core.Model
+{
+    /*
+    A frequent flyer number consists of 2 parts separated by a '-':
+    [member number]-[scheme identifier]
+    Member numbers consist of 6 numeric digits
+    Scheme identifiers are a single uppercase alphabetic character
+    */
+
+    public class FrequentFlyerNumberValidator
+    {
+        private readonly char[] _validSchemeIdentifiers = { 'A', 'Q', 'Y' };
+        private const int ExpectedTotalLegnth = 8;
+        private const int ExpectedMemberNumberLength = 6;
+
+        public bool IsValid(string frequentFlyerNumber)
+        {
+            if(frequentFlyerNumber is null)
+            {
+                throw new ArgumentNullException(nameof(frequentFlyerNumber));
+            }
+
+            if(frequentFlyerNumber.Length != ExpectedTotalLegnth)
+            {
+                return false;
+            }
+
+            var memberNumberPart = frequentFlyerNumber.Substring(0, ExpectedMemberNumberLength);
+            if(!int.TryParse(memberNumberPart, NumberStyles.None, null, out int _))
+            {
+                return false;
+            }
+
+            var schemeIdentifier = frequentFlyerNumber.Last();
+            return _validSchemeIdentifiers.Contains(schemeIdentifier);
+
+        }
+    }
+}
