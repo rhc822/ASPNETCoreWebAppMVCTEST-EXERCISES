33aabaff113a7dfed0772673345f78da481cb7cb
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-10-06 15:55:17 -0500

    Created ApplyControllerShould test

---
+++ tests/CreditCard.Tests/Controller/ApplyControllerShould.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using CreditCards.Controllers;
+using CreditCards.Core.Interfaces;
+using CreditCards.Core.Model;
+using CreditCards.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace CreditCard.Tests.Controller
+{
+    public class ApplyControllerShould
+    {
+        private readonly Mock<ICreditCardApplicationRepository> _mockRepository;
+        private readonly ApplyController _sut;
+
+        public ApplyControllerShould()
+        {
+            _mockRepository = new Mock<ICreditCardApplicationRepository>();
+            _sut = new ApplyController(_mockRepository.Object);
+        }
+
+        [Fact]
+        public void ReturnViewForIndex()
+        {
+            IActionResult result = _sut.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task ReturnViewWhenInvalidModelState()
+        {
+            _sut.ModelState.AddModelError("x", "Test Error");
+
+            var application = new NewCreditCardApplicationDetails
+            {
+                FirstName = "Sarah"
+            };
+
+            IActionResult result = await _sut.Index(application);
+
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = Assert.IsType<NewCreditCardApplicationDetails>(viewResult.Model);
+
+            Assert.Equal(application.FirstName, model.FirstName);
+        }
+    }
+}
