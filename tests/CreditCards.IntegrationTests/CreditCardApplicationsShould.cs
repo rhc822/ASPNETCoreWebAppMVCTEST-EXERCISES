44414565041f73e07e4ea54ee79609eb16c37617
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-10-08 09:48:30 -0500

    Initial set up and dependencies for Module 6: Writing ASP.NET Core MVC Integration Tests (had to manually add PackageReference to TestHost in csproj files)

---
+++ tests/CreditCards.IntegrationTests/CreditCardApplicationsShould.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Xunit;
+
+namespace CreditCards.IntegrationTests
+{
+    public class CreditCardApplicationsShould
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
